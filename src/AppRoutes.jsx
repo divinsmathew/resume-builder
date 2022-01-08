ac97b6330dc8373337c41c119ae93c30ca8b4fa9
Divins Mathew <divinsmathew@gmail.com>
2022-01-08 17:22:43 +0530

    github pages config

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 export default function AppRoutes() {
+    console.log(process.env.PUBLIC_URL);
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<App initialMode="create" />} />
                 <Route path="/create" element={<App initialMode="create" />} />
